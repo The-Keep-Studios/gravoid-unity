d26e406da183e60cf8d48e9722cbac8059e50602
Ian T. Small <ian.small@thekeepstudios.com>
2014-05-25 15:33:31 -0400

    Simplified the event registration be creating a SelfRegisteringEventListener class

---
+++ Assets/Plugins/The Keep Studios/listeners/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace TheKeepStudios.listeners {
+	/// <summary>
+	/// Scene loader which loads a given scene when onEvent is triggered.
+	/// </summary>
+	public class SceneLoader : events.SelfRegisteringEventListener{
+		public string sceneToLoad;
+		
+		override public EventHandler OnEvent {
+			get {
+				return LoadScene;
+			}
+		}
+
+		private void LoadScene(object source, EventArgs e){
+			Application.LoadLevel(sceneToLoad);
+		}
+	}
+}
