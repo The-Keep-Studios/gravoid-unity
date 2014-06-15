abb1b7dead2154e8b8765a7f6dc04864b3db2785
Ian T. Small <ian.small@thekeepstudios.com>
2014-06-14 23:54:11 -0400

    Initial server creation and joining. Created semi-hard coded server creation and joining. GUI creation has been started but is unfinished and is purely visual.

---
+++ Assets/Plugins/The Keep Studios/network/ClientManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TheKeepStudios.network
+{
+	public class ClientManager : MonoBehaviour
+	{
+
+		private HostData[] hostList = new HostData[0];
+		private bool isRefreshingHostList = false;
+		public GameObject playerPrefab;
+		public int playerSpawnGroup;
+
+		void OnGUI ()
+		{
+			if (!Network.isClient && !Network.isServer) {
+				GUILayout.BeginArea (new Rect (0, 21, 200, 200));
+			
+				if (GUILayout.Button ("Refresh Hosts")) {
+					RefreshHostList ();
+				}
+
+				GUILayout.BeginScrollView (new Vector2 (190, 190));
+				foreach (HostData host in hostList) {
+					if (GUILayout.Button (host.gameName)) {
+						JoinServer (host);
+					}
+				}
+				GUILayout.EndScrollView ();
+				GUILayout.EndArea ();
+			}
+		}
+	
+		void Update ()
+		{
+			if (isRefreshingHostList && MasterServer.PollHostList ().Length > 0) {
+				isRefreshingHostList = false;
+				hostList = MasterServer.PollHostList ();
+			}
+		}
+	
+		private void RefreshHostList ()
+		{
+			if (!isRefreshingHostList) {
+				isRefreshingHostList = true;
+				MasterServer.RequestHostList (ApplicationValues.Name);
+			}
+		}
+	
+		public void JoinServer (HostData hostData)
+		{
+			Network.Connect (hostData);
+		}
+	
+		void OnConnectedToServer ()
+		{
+			SpawnPlayer ();
+		}
+	
+		private void SpawnPlayer ()
+		{
+			Network.Instantiate (playerPrefab, Vector3.zero, Quaternion.identity, playerSpawnGroup);
+		}
+	}
+}
---
+++ Assets/Plugins/The Keep Studios/network/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TheKeepStudios.network
+{
+	public class NetworkManager : MonoBehaviour
+	{
+
+		public string typeName = "UniqueGameName";
+	
+		// Use this for initialization
+		void Start (){
+		
+		}
+		
+		void OnGUI () {
+			if (!Network.isClient && !Network.isServer && GUI.Button (new Rect (0, 0, 200, 20), "Start Server")) {
+				StartServer ();
+			}
+		}
+		
+		private void StartServer ()
+		{
+			Network.InitializeServer (5, 25000, !Network.HavePublicAddress ());
+			MasterServer.RegisterHost (typeName, ApplicationValues.Name);
+		}
+		
+		void OnServerInitialized ()
+		{
+			//SpawnPlayer ();
+		}
+	}
+}
