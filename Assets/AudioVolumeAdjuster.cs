aadbd9be3585559f453423e3e577f1c0d629977a
Ian T. Small <ian.small@thekeepstudios.com>
2014-09-13 16:39:15 -0400

    Implemented new menu

---
+++ Assets/AudioVolumeAdjuster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioVolumeAdjuster : MonoBehaviour{
+
+	// Use this for initialization
+	void Start(){
+	
+	}
+	
+	// Update is called once per frame
+	void Update(){
+	
+	}
+
+	public void ChangeMasterAudioVolumn(float newLevel){
+		AudioListener.volume = newLevel;
+	}
+}
--- Assets/Plugins/The Keep Studios/events/listeners/SceneLoader.cs
+++ Assets/Plugins/The Keep Studios/events/listeners/SceneLoader.cs
@@ -2,21 +2,25 @@
 using System.Collections;
 using System;
 
-namespace TheKeepStudios.events.listeners {
+namespace TheKeepStudios.events.listeners{
 	/// <summary>
 	/// Scene loader which loads a given scene when onEvent is triggered.
 	/// </summary>
 	public class SceneLoader : SelfRegisteringEventListener{
 		public string sceneToLoad;
 		
-		override public EventHandler OnEvent {
-			get {
+		override public EventHandler OnEvent{
+			get{
 				return LoadScene;
 			}
 		}
 
-		private void LoadScene(object source, EventArgs e){
+		public void LoadScene(){
 			Application.LoadLevel(sceneToLoad);
+		}
+
+		public void LoadScene(object source, EventArgs e){
+			LoadScene();
 		}
 	}
 }
