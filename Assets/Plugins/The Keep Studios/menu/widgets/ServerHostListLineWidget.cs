b198f5f88c8b8dc8033f27033ee7e3a36e583763
Ian T. Small <ian.small@thekeepstudios.com>
2014-09-13 19:12:35 -0400

    Created server browser

---
+++ Assets/Plugins/The Keep Studios/menu/widgets/ServerHostListLineWidget.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TheKeepStudios.menu.widgets{
+
+	public class ServerHostListLineWidget : MonoBehaviour{
+
+		public TheKeepStudios.network.ServerJoiner serverJoiner;
+
+		public HostData Host{
+			get{
+				return serverJoiner.hostToJoin;
+			}
+			set{
+				serverJoiner.hostToJoin = value;
+				gameObject.SetActive(Host != null);//widget is active iff we have a host
+				if(Host != null){
+					UpdateGUIElements();
+				}
+			}
+		}
+
+		private void UpdateGUIElements(){
+			UnityEngine.UI.Text text = this.GetComponentInChildren<UnityEngine.UI.Text>();
+			text.text = Host.gameName;
+		}
+	}
+}
--- Assets/Plugins/The Keep Studios/menu/widgets/ServerHostListWidget.cs
+++ Assets/Plugins/The Keep Studios/menu/widgets/ServerHostListWidget.cs
@@ -3,35 +3,32 @@ using System;
 using System.Collections;
 
 namespace TheKeepStudios.menu.widgets{
-	public class ServerHostListWidget : Widget{
+
+	public class ServerHostListWidget : MonoBehaviour{
 
 		public TheKeepStudios.network.ClientManager clientManager;
-		public Vector2 scrollPosition;
-	
-		override public void Draw (){
-			if (!Network.isClient && !Network.isServer) {			
-				if (GUILayout.Button ("Refresh Hosts")) {
-					StartCoroutine (RefreshHostList(this.clientManager));
-				}
-				
-				GUILayout.BeginScrollView (scrollPosition);
-				foreach (HostData host in this.clientManager.HostList) {
-					if (GUILayout.Button (host.gameName)) {
-						StartCoroutine (JoinServer (this.clientManager, host));
-					}
-				}
-				GUILayout.EndScrollView ();
-			}
+
+		public ServerHostListLineWidget serverDisplayPrefab;
+
+		void Start(){
+			Refresh();
 		}
 
-		private IEnumerator JoinServer(TheKeepStudios.network.ClientManager client, HostData host){
-			yield return null; //ensure we are NOT on the GUI update frame
-			client.JoinServer(host);
+		public void Refresh(){
+			StartCoroutine(RefreshHostList(this.clientManager));
 		}
 
 		private IEnumerator RefreshHostList(TheKeepStudios.network.ClientManager client){
 			yield return null; //ensure we are NOT on the GUI update frame
 			client.RefreshHostList();
+			ServerHostListLineWidget[] displays = GetComponentsInChildren<ServerHostListLineWidget>();
+			HostData[] hosts = this.clientManager.HostList;
+			for(int hostIdx = 0; hostIdx < hosts.Length || hostIdx < displays.Length; ++hostIdx){
+				HostData host = hostIdx < hosts.Length ? hosts[hostIdx] : null; //get the host data if it exists
+				ServerHostListLineWidget widget = hostIdx < displays.Length ? displays[hostIdx] : ((GameObject)Instantiate(serverDisplayPrefab)).GetComponent<ServerHostListLineWidget>(); //get or make the matching widget
+				widget.transform.SetParent(this.gameObject.transform, false); //set ourselves as the parent object for the new widget
+				widget.Host = host;//update the widget host
+			}
 		}
 
 	}
