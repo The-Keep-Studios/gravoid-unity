d6ac7b98349ffe9bb7fb28eaf95ecbcd1a7b6c76
Ian T. Small <ian.small@thekeepstudios.com>
2014-11-29 16:27:36 -0500

    Finishes GRA-294 and starts GRA-300 Modified CUBS Configuration to closer to final layout

---
+++ Assets/gravoid/scripts/CUBS/CUBSConfigWidgetController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace TheKeepStudios.Gravoid{
+
+	public class CUBSConfigWidgetController : MonoBehaviour{
+
+		// Use this for initialization
+		void Start(){
+	
+		}
+	
+		// Update is called once per frame
+		void Update(){
+	
+		}
+	}
+}
