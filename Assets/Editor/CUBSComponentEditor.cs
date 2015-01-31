d06abcb8a62aeaf8e81e7c8004b2da92be52e70f
Benjamin Mishler <benjamin.mishler@thekeepstudios.com>
2015-01-31 13:38:25 -0500

    Updated all of the component prefabs so that their buttons in the CUBS UI will show their info when selected. POSSIBLE BUG: The Text provided for each object may clear every time the scene is loaded.

--- Assets/Editor/CUBSComponentEditor.cs
+++ Assets/Editor/CUBSComponentEditor.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
-using TheKeepStudios.Gravoid.CUBS.Ballistics;
+// using TheKeepStudios.Gravoid.CUBS.Ballistics;
 
-[CustomEditor(typeof(PartSelectionBehavior))]
+[CustomEditor(typeof(TheKeepStudios.Gravoid.CUBS.Ballistics.PartSelectionBehavior))]
 public class CUBSComponentEditor : Editor {
 	public override void OnInspectorGUI()
 	{
-		PartSelectionBehavior myPartSelectionBehavior = (PartSelectionBehavior)target;
+		TheKeepStudios.Gravoid.CUBS.Ballistics.PartSelectionBehavior myPartSelectionBehavior = (TheKeepStudios.Gravoid.CUBS.Ballistics.PartSelectionBehavior)target;
 
-		myPartSelectionBehavior.Icon = (Sprite)EditorGUILayout.ObjectField("Icon Sprite", myPartSelectionBehavior.Icon, typeof(GameObject), false);
+		myPartSelectionBehavior.Icon = EditorGUILayout.ObjectField("Icon Sprite", myPartSelectionBehavior.Icon, typeof(Sprite), false) as Sprite;
 		myPartSelectionBehavior.Title = EditorGUILayout.TextField("Object Name", myPartSelectionBehavior.Title);
 		myPartSelectionBehavior.Description = EditorGUILayout.TextField("Object Description", myPartSelectionBehavior.Description);
 		myPartSelectionBehavior.Flavor = EditorGUILayout.TextField("Object Flavor Text", myPartSelectionBehavior.Flavor);
