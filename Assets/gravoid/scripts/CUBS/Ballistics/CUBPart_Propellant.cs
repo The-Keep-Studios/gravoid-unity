6d895ef2cf85a326ad1426f70f1f5f1e93fdc83c
Benjamin Mishler <benjamin.mishler@thekeepstudios.com>
2015-02-22 11:33:36 -0500

    Partially coded the propellant part, Ian will finish the coding

--- Assets/gravoid/scripts/CUBS/Ballistics/CUBPart_Propellant.cs
+++ Assets/gravoid/scripts/CUBS/Ballistics/CUBPart_Propellant.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using System.Collections;
 
+//TODO Needs to know which way to force things
+//TODO What happens if it is not yet atached to something?
+//FIXME Need to know how the grabber is handling "attatching" to items. Is it all one rigidbody?
+
 namespace TheKeepStudios.Gravoid.CUBS.Ballistics{
 	
 	public class CUBPart_Propellant : CUBPartBase
@@ -13,9 +17,50 @@ namespace TheKeepStudios.Gravoid.CUBS.Ballistics{
 		[SerializeField]
 		float force;
 
+		//is the rocket firing?
+		private bool isFiring = false;
+		//is used as a counter to track how much time has passed while the physics is updating.
+		private float timeCounter = 0;
+		private float currentTime;
+
+
 
 		override public void Activate(GameObject activator){
 		
+		}
+
+		//When the part is activated it will call this function
+		[ContextMenu("Light this candle")]
+		private void IgnitePropellant()
+		{
+			Debug.Log ("the propellent should be active");
+			isFiring = true;
+
+
+		}
+
+
+		void FixedUpdate()
+		{
+			//Is the engine firing?
+			if (isFiring)
+			{
+				//Taking the current position
+				currentTime = (++timeCounter)*Time.fixedDeltaTime;
+
+				if(currentTime <= burnTime)
+				{
+					//transform.parent.rigidbody
+					Debug.Log ("Current timestep" +currentTime );
+
+
+
+
+				}
+			}
+
+
+
 		}
 
 
