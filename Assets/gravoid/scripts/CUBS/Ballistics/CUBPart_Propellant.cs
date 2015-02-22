a44afed66b514d458a7fa22a618d813f1da582f2
Benjamin Mishler <benjamin.mishler@thekeepstudios.com>
2015-02-22 12:49:02 -0500

    Finished the EXPLOSIONS?!?!?! code (also slightly raised the mass of the iron asteroid BECAUSE REASONS!!) --This commit message is brought to you by Torgue.

--- Assets/gravoid/scripts/CUBS/Ballistics/CUBPart_Propellant.cs
+++ Assets/gravoid/scripts/CUBS/Ballistics/CUBPart_Propellant.cs
@@ -5,68 +5,65 @@ using System.Collections;
 //TODO What happens if it is not yet atached to something?
 //FIXME Need to know how the grabber is handling "attatching" to items. Is it all one rigidbody?
 
-namespace TheKeepStudios.Gravoid.CUBS.Ballistics{
-	
-	public class CUBPart_Propellant : CUBPartBase
-	{
-		//The amount of time for which the engine will burn
-		[SerializeField]
-		float burnTime;
-
-		//The force which will be imparted on the object
-		[SerializeField]
-		float force;
-
-		//is the rocket firing?
-		private bool isFiring = false;
-		//is used as a counter to track how much time has passed while the physics is updating.
-		private float timeCounter = 0;
-		private float currentTime;
-
-
-
-		override public void Activate(GameObject activator){
-		
-		}
-
-		//When the part is activated it will call this function
-		[ContextMenu("Light this candle")]
-		private void IgnitePropellant()
+namespace TheKeepStudios.Gravoid.CUBS.Ballistics
+{
+		public class CUBPart_Propellant : CUBPartBase
 		{
-			Debug.Log ("the propellent should be active");
-			isFiring = true;
+				//The amount of time for which the engine will burn
+				[SerializeField]
+				float burnTime;
 
+				//The force which will be imparted on the object
+				[SerializeField]
+				float
+						force;
 
-		}
+				//is the rocket firing?
+				private bool isFiring = false;
+				//is used as a counter to track how much time has passed while the physics is updating.
+				private float timeCounter = 0;
+				private float currentTime;
 
-
-		void FixedUpdate()
-		{
-			//Is the engine firing?
-			if (isFiring)
-			{
-				//Taking the current position
-				currentTime = (++timeCounter)*Time.fixedDeltaTime;
-
-				if(currentTime <= burnTime)
+				override public void Activate (GameObject activator)
 				{
-					//transform.parent.rigidbody
-					Debug.Log ("Current timestep" +currentTime );
+		
+				}
 
+				//When the part is activated it will call this function
+				[ContextMenu("Light this candle")]
+				private void IgnitePropellant ()
+				{
+						Debug.Log ("the propellent should be active");
+						isFiring = true;
+
+
+				}
+
+				void FixedUpdate ()
+				{
+						//Is the engine firing?
+						if (isFiring) {
+								//Taking the current position
+								currentTime = (++timeCounter) * Time.fixedDeltaTime;
+
+								if (currentTime <= burnTime) {
+										//transform.parent.rigidbody
+										Debug.Log ("Current timestep" + currentTime);
+
+
+
+
+								}
+						}
 
 
 
 				}
-			}
-
-
-
-		}
 
 
 
 
 		
-	}
+		}
 }
 
