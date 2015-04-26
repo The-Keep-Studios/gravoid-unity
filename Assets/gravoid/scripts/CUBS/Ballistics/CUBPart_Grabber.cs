0691ee270a70f366350f8a7431dc3e253c12daa7
Ian T. Small <ian.small@thekeepstudios.com>
2015-04-26 15:23:21 -0400

    Added grab angle arc for grabber CUB part

--- Assets/gravoid/scripts/CUBS/Ballistics/CUBPart_Grabber.cs
+++ Assets/gravoid/scripts/CUBS/Ballistics/CUBPart_Grabber.cs
@@ -4,13 +4,22 @@ using System.Collections;
 namespace TheKeepStudios.Gravoid.CUBS.Ballistics{
 
 	public class CUBPart_Grabber : CUBPart{
+
+		[UnityEngine.SerializeField]
+		float grabConeAngle;
+
 		override public void Activate(GameObject activator){
 			
 		}
 		
 		override public void OnCollisionEnter(Collision collision){
 			base.OnCollisionEnter(collision);
-			GrabObject(collision.gameObject);
+			if (collsionInGrabArc(collision)) {
+				GrabObject(collision.gameObject);
+			}
+			else {
+				Debug.Log("Not grabbing object " + collision.gameObject + " with " + gameObject + " as it is not in the grab arc");
+			}
 		}
 		
 		private void GrabObject(GameObject objectToGrab){
@@ -29,6 +38,23 @@ namespace TheKeepStudios.Gravoid.CUBS.Ballistics{
 				}
 			}
 		}
-		
+
+		private bool collsionInGrabArc(Collision collision) {
+			foreach (ContactPoint contact in collision.contacts) {
+				if(pointInGrabTriggerArea(contact.point)){
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private bool pointInGrabTriggerArea(Vector3 point) {
+			return angleFromFront(point) <= grabConeAngle;
+		}
+
+		private float angleFromFront(Vector3 point) {
+			return Vector3.Angle(transform.forward, transform.position - point);
+		}
+
 	}
 }
